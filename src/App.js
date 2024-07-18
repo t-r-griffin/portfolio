b18eed787e4e3ae2fd586e8bd99efca7f0cf35c4
Tom Griffin <westonroadflows28@gmail.com>
2024-07-18 15:41:13 +0100

    Add three.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-      <Header />
-      <HeroSection />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </>
+    <div>
+      3D Developer Portfolio
+    </div>
   );
 }
 
